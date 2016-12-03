a10a5faa3ab9a24f7752813c244ffb7fd9dc9e7d
Hai Zhu <zhuh1106@gmail.com>
2016-12-03 15:27:27 -0500

    update unittest, testdata for SLP

--- python_code/unitTests.py
+++ python_code/unitTests.py
@@ -1,6 +1,9 @@
 import unittest
 import numpy as np
+import scipy.io as sio
 from quadr import quadr
+from matplotlib import path
+from LapSLPmatrix import LapSLPmatrix
 
 class TestQuadr(unittest.TestCase):
 
@@ -34,6 +37,69 @@ class TestQuadr(unittest.TestCase):
         t = quadr(s)
         # error in normal vec, should be small
         self.assertTrue(np.linalg.norm(t['nx']-s['nx']) <= np.finfo(float).eps)
+        
+        
+class TestLapSLP(unittest.TestCase):
+    
+    def setUp(self):
+        
+        N              = 500
+        # source: starfish domain
+        a              = 0.3
+        w              = 5
+        R              = lambda t: (1 + a*np.cos(w*t))*1
+        Rp             = lambda t: -w*a*np.sin(w*t)
+        Rpp            = lambda t: -w*w*a*np.cos(w*t)
+        self.s         = {}
+        self.s['Z']    = lambda t: R(t)*np.exp(1j*t)
+        self.s['Zp']   = lambda t: (Rp(t) + 1j*R(t))*np.exp(1j*t)
+        self.s['Zpp']  = lambda t: (Rpp(t) + 2j*Rp(t) - R(t))*np.exp(1j*t)
+        self.s         = quadr(self.s,N)
+        
+        # target
+        nx             = 100
+        gx             = (np.arange(1,nx+1)/nx*2-1)*1.5
+        ny             = 100
+        gy             = (np.arange(1,ny+1)/ny*2-1)*1.5     # set up plotting
+        xx, yy         = np.meshgrid(gx,gy)
+        zz             = xx + 1j*yy
+        
+        self.t         = {}
+        p              = path.Path(np.vstack((np.real(self.s['x']).T,np.imag(self.s['x']).T)).T)
+        ii             = ~p.contains_points(np.vstack((np.real(zz).flatten('F'),np.imag(zz).flatten('F'))).T)
+        self.t['x']    = zz.flatten('F')[ii][np.newaxis].T
+        
+        # bad target
+        self.tbad      = {}
+        self.tbad['x'] = self.s['x']
+        
+        
+        # precision 
+        self.eps       = 10**(-14)      # An won't accept smaller precision...
+        
+        # test data dir
+        import os 
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.dir       = dir_path + "/TestData/"
+    
+        
+    # check that when input target is bad (normal derivative wasn't provided)
+    def test_badInput(self):
+        with self.assertRaises(Exception):
+            _, An = LapSLPmatrix(self.tbad,self.s,0,1)
+        
+    
+    # check that SLPmatrix A in python and matlab are the same        
+    def test_matlabEqualA(self):
+        test      = sio.loadmat(self.dir+'testA.mat')
+        A         = LapSLPmatrix(self.t,self.s,0,0)              
+        self.assertTrue(np.abs(np.max(A-test['A'])) <= self.eps)
+    
+    # check that normal derivative An in python and matlab are the same
+    def test_matlabEqualAn(self):
+        test      = sio.loadmat(self.dir+'testAn.mat')
+        _, An     = LapSLPmatrix(self.s,self.s,0,1)
+        self.assertTrue(np.abs(np.max(An-test['An'])) <= self.eps)
 
 
 if __name__ == '__main__':
